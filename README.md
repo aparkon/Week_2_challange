862f3fbb3d8b1b5830d2b771e96b2e115e8b850a
finn <aparkon@gmail.com>
2021-07-11 17:46:03 -0400

    1.1 updates

--- README.md
+++ README.md
@@ -1,1 +1,46 @@
-# week_2_challange
+# Project Title
+
+Just after the title, introduce your project by describing attractively what the project is about and what is the main problem that inspires you to create this project or what is the main contribution for the potential user of your project.
+
+---
+The application is about qualifiyng application's qualifition for mortgage.  There is certain criteria to be qualified for the loan.
+1. Credit score
+2. LTV
+3. DTI
+4. Max loan amount 
+
+in the software I have developed, It should run the applicants personal information and match it with the banks qualifying analysis according to the data 
+## Technologies
+
+Describe the technologies required to use your project such as programming languages, libraries, frameworks, and operating systems. Be sure to include the specific versions of any critical dependencies that you have used in the stable version of your project.
+
+---
+The program 
+
+## Installation Guide
+
+In this section, you should include detailed installation notes containing code blocks and screenshots.
+
+---
+![](C2_examples.png)
+## Examples
+
+This section should include screenshots, code blocks, or animations showing how your project works.
+![](C2_examples.png)
+---
+
+## Usage
+
+This section should include screenshots, code blocks, or animations explaining how to use your project.
+
+---
+
+## Contributors
+
+In this section, list all the people who contribute to this project; since you may want to be reached by recruiters or potential collaborators, include your contact e-mail, and optionally your LinkedIn or Twitter profile.
+
+---
+
+## License
+
+When you share a project on a repository, especially a public one, it's important to choose the right license to specify others what they can and can not do with your source code and files. Use this section to include the licence you want to use.
---
+++ tests/test_qualifier.py
@@ -0,0 +1,67 @@
+# Import pathlib
+
+from pathlib import Path
+
+#Import fileio
+
+from qualifier.utils.fileio import load_csv,save_csv
+# Import Calculators
+from qualifier.utils import calculators
+
+# Import Filters
+from qualifier.filters import credit_score
+from qualifier.filters import debt_to_income
+from qualifier.filters import loan_to_value
+from qualifier.filters import max_loan_size
+
+
+def test_save_csv():
+    # @TODO: Your code here!
+  
+    # Use Path from pathlib to output the test csv to ./data/output/qualifying_loans.csv
+    csvpath = Path("./data/output/qualifying_loans.csv")
+    save_csv(csvpath, data, head=None)
+    assert test_save_csv()
+
+
+#def save_csv(csvpath, data, header=None):
+#    
+#    with open(csvpath, "w",newline="") as csvfile:
+#        csvwriter = csv.writer(csvfile,delimiter=",")
+#        if header:
+#            csvwriter.writerow(header)
+#        csvwriter.writerow(data)
+
+#def load_bank_data():
+#    csvpath = questionary.text("Enter a file path to a rate-sheet (.csv):").ask()
+#    csvpath = Path(csvpath)
+#    if not csvpath.exists():
+#        sys.exit(f"Oops! Can't find this path: {csvpath}")
+#    return load_csv(csvpath)
+
+def test_calculate_monthly_debt_ratio():
+    assert calculators.calculate_monthly_debt_ratio(1500, 4000) == 0.375
+
+def test_calculate_loan_to_value_ratio():
+    assert calculators.calculate_loan_to_value_ratio(210000, 250000) == 0.84
+
+def test_filters():
+    bank_data = fileio.load_csv(Path('./data/daily_rate_sheet.csv'))
+    current_credit_score = 750
+    debt = 1500
+    income = 4000
+    loan = 210000
+    home_value = 250000
+
+    monthly_debt_ratio = 0.375
+
+    loan_to_value_ratio = 0.84
+
+    # @TODO: Test the new save_csv code!
+    bank_data_filtered = max_loan_size.filter_max_loan_size(loan,bank_data)
+    bank_data_filtered = loan_to_value.filter_loan_to_value(loan_to_value_ratio, bank_data_filtered)
+    bank_data_filtered = credit_score.filter_credit_score(credit_score, bank_data_filtered)
+    bank_data_filtered = debt_to_income.filter_debt_to_income(monthly_debt_ratio, bank_data_filtered)
+
+
+    assert test_save_csv.exists()
